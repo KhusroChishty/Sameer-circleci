f681c1fa23c3d34a1d17c2935440db6bdeddf505
KhusroChishty <114980329+KhusroChishty@users.noreply.github.com>
2024-11-01 12:25:40 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,14 +7,3 @@ def create_app():
     app.register_blueprint(main)
 
     return app
-fetch('/config.json')
-  .then(response => response.json())
-  .then(config => {
-    const apiUrl = config.apiEndpoint;
-
-    // Example usage in a fetch request
-    fetch(`${apiUrl}/endpoint`)
-      .then(response => response.json())
-      .then(data => console.log(data));
-  })
-  .catch(error => console.error('Error loading config:', error));
